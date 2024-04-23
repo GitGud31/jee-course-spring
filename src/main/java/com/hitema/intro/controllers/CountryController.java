23b3b17195258d682386ff4ffeefa25618981b3f
GitGud31 <mhamedlehbab@gmail.com>
2024-04-23 14:41:15 +0200

    added post mapping

--- src/main/java/com/hitema/intro/controllers/CountryController.java
+++ src/main/java/com/hitema/intro/controllers/CountryController.java
@@ -5,10 +5,7 @@ import com.hitema.intro.services.CountryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +29,10 @@ public class CountryController {
     @GetMapping("/{id}")
     public Country getOne(@PathVariable Long id) {
         return this.service.read(id);
+    }
+
+    @PostMapping("/create")
+    public Country createCountry(@RequestBody Country country) {
+        return this.service.create(country);
     }
 }
